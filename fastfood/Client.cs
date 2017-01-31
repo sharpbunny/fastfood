bc4ae2a11dad17da06b2088f641bf6dd82b7782a
Zerdiron <sanchezdamien34@hotmail.fr>
2017-01-31 16:00:08 +0100

    ajout de commentaire dans class client

--- fastfood/Client.cs
+++ fastfood/Client.cs
@@ -9,16 +9,28 @@ namespace fastfood
 	class Client
 	{
 		// Attributs.
+		/// <summary>
+		/// Identifiant du client.
+		/// </summary>
 		private int _IDClient;
 
 		// Constructeurs.
 
 		// Getters & Setters.
+		/// <summary>
+		/// Retourne l'id du client.
+		/// </summary>
 		public int IDClient
 		{
 			get	{ return _IDClient; }
 		}
 
+		/// <summary>
+		/// Lors du premier choix d'un article créer un tableau de choix puis ajoute les choix ultérieurs dans le tableau.
+		/// </summary>
+		/// <param name="tabArticle">Tableau d'objet Article.</param>
+		/// <param name="newArticle">Le nouvel aticle selectionné par le client.</param>
+		/// <returns>Renvoie le tableau d'article complété.</returns>
 		// Fonctions.
 		public Article[] ChoixArticle(Article[]tabArticle, Article newArticle)
 		{
@@ -31,6 +43,10 @@ namespace fastfood
 			return tabArticle;
 		}
 
+		/// <summary>
+		/// Permet au client de payer sa commande.
+		/// </summary>
+		/// <returns>Renvoie true si le paiement est valide. On peut donc passer à la préparation de la commande.</returns>
 		public bool Payer()
 		{
 			bool paiementValide = false;
