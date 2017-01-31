8430178f9e0791dec47941c62e5863192f698d37
jringenbach <34011-14-08@exchange.ad.afpanet>
2017-01-31 15:09:33 +0100

    Création de la classe Commande.cs

---
+++ fastfood/Commande.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace fastfood
+{
+	class Commande
+	{
+		static private int _numero { get; set; }
+		private string _type { get; set; }
+
+											// **********************************
+											//			CONSTRUCTEURS
+											// **********************************
+
+		public Commande()
+		{
+			_numero++; //Le numéro de commande est incrémenté à chaque création de Commande
+		}
+
+											// **********************************
+											//				METHODES
+											// **********************************
+		
+	}
+}
