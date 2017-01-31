69c8b8a36cdfb1a8dbb4dda00720029ae2cfb120
Juliengng <gagniare.j@gmail.com>
2017-01-31 15:38:58 +0100

    modification Personnel.cs

--- fastfood/Personnel.cs
+++ fastfood/Personnel.cs
@@ -8,6 +8,42 @@ namespace fastfood
 {
     abstract class Personnel
     {
+        //attributs
+        private string _nom;
+        private string _prenom;
 
+
+        //accesseur et muttateur
+        public string Nom
+        {
+            get
+            {
+                return _nom;
+            }
+
+            set
+            {
+                _nom = value;
+            }
+        }
+        public string Prenom
+        {
+            get
+            {
+                return _prenom;
+            }
+
+            set
+            {
+                _prenom = value;
+            }
+        }
+
+        //methode
+
+        public void ConsultePlanning()
+        {
+            Console.WriteLine(" consulte le planning ");
+        }
     }
 }
