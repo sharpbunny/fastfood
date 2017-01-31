ada8bcff2f4fee1810b6c8528a3b42757824b983
Zerdiron <sanchezdamien34@hotmail.fr>
2017-01-31 15:17:56 +0100

    add basic function for Client

--- fastfood/Client.cs
+++ fastfood/Client.cs
@@ -8,6 +8,28 @@ namespace fastfood
 {
 	class Client
 	{
+		// Attributs.
+		private int IDClient;
 
+		// Constructeurs.
+
+		// Getters & Setters.
+		public int IDClient1
+		{
+			get	{ return IDClient; }
+		}
+
+		// Fonctions.
+		public Article[] ChoixArticle()
+		{
+			Article[] tabArticle;
+			return tabArticle;
+		}
+
+		public bool Payer()
+		{
+			bool paiementValide = false;
+			return paiementValide;
+		}
 	}
 }
