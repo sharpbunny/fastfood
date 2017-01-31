9e5f0b5a3145a74cb53a2cd28a71407fdd4e216e
FRANCE Frédéric <frederic.france@free.fr>
2017-01-31 16:14:42 +0100

    add manager

---
+++ fastfood/Manager.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace fastfood
+{
+	class Manager:Personnel
+	{
+		//Attribut
+
+		//Constructeur
+
+		//Methode
+
+		//Accesseur et Mutateur
+	}
+}
