e5eeb704dc2d649eacc7143e0080d5ab8f09c56d
jringenbach <34011-14-08@exchange.ad.afpanet>
2017-01-31 15:27:00 +0100

    Ajout de la documentation pour la classe Commande.cs

--- fastfood/Commande.cs
+++ fastfood/Commande.cs
@@ -8,13 +8,18 @@ namespace fastfood
 {
 	class Commande
 	{
+		/// <summary>
+		/// Déclaration des variables
+		/// </summary>
 		static private int _numero { get; set; }
 		private string _type { get; set; }
 
 											// **********************************
 											//			CONSTRUCTEURS
 											// **********************************
-
+	/// <summary>
+	/// Constructeur de la classe Commande
+	/// </summary>
 		public Commande()
 		{
 			_numero++; //Le numéro de commande est incrémenté à chaque création de Commande
