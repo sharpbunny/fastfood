a4bd338f1232678ec55d3eed58145c7f1e1e9ddc
Vona <vona.chum@gmail.com>
2017-02-01 09:43:33 +0100

    MAJ de la classe caissier pour la methode EncaisserEspece

--- fastfood/Caissier.cs
+++ fastfood/Caissier.cs
@@ -7,18 +7,57 @@ using System.Threading.Tasks;
 namespace fastfood
 {
     class Caissier:Personnel
-    {   
+    {
         // Attributs.
+        // Attributs pour EncaisserEspece
+        int saissirEspeceClient = 0;
+        int sommeTotalCommande = 0;
+        int sommeRestant = 0; //Apres avoir compter le nimbre de billet de 10E à rendre
+        int compteur10E = 0;
+        int compteur5E = 0;
+        int sommeARendreEnPiece = 0;
 
         // Constructeurs.
 
         // Accesseurs.   
-        
-        // Fonctions.
+
+        // Methodes.
         /// <summary>
-		/// Methode pour l'encaissement
-		/// </summary>
-        public void Encaisser()
+        /// Paiement en especes par le client
+        /// </summary>
+        public void EncaisserEspece()
+        {
+            
+            int sommeARendre = saissirEspeceClient - sommeTotalCommande;
+
+            if(sommeARendre>0)
+            {
+                
+                while (sommeARendre > 10)
+                {
+                    sommeRestant = sommeARendre - 10;
+                    compteur10E++;                    
+                }
+                if (sommeRestant > 5)
+                {
+                    sommeARendreEnPiece = sommeRestant - 5;
+                    compteur5E++; 
+                }
+                    
+            }
+            
+        }
+        /// <summary>
+        /// Paiement en carte bancaire par le client
+        /// </summary>
+        public void EncaisserCB()
+        {
+
+        }
+        /// <summary>
+        /// Paiement cheque par la client
+        /// </summary>
+        public void EncaisserCheque()
         {
             
         }
