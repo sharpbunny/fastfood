97a4899b6825bcd493f46d6eca06b1efeb36ea14
jringenbach <34011-14-08@exchange.ad.afpanet>
2017-02-02 16:19:16 +0100

    Implémentation de ChoixDeLUtilisateur, MenuPrincipal, ChoixArticle)

--- fastfood/Program.cs
+++ fastfood/Program.cs
@@ -18,94 +18,57 @@ namespace fastfood
             return Convert.ToInt32(Console.ReadLine());
         }
 
-		/// <summary>
-		/// Choix de l'utilisateur : fonction utile au développeur pour se mettre dans la position du client ou du personnel de Végétafood
-		/// </summary>
-        static void affichageMenuProg()
-        {
-            Console.WriteLine("Bonjour et bienvenue dans VégétaFood !");
-            Console.WriteLine("Veuillez nous indiquer si vous êtes client ou si vous faites parti du personnel :");
-			Console.WriteLine(" 1.Client");
-			Console.WriteLine(" 2.Personnel");
-			Console.WriteLine(" 3.Debug");
 
-            int nombre = lireEntier();
-            switch (nombre)
-            {
-                case 1:
-                    Console.WriteLine("Vous êtes authentifé en tant que client.");
-                    break;
-                case 2:
-                    Console.WriteLine("Vous faites parti du personnel.\nVeuillez saisir le login : ");
-                    if (Console.ReadLine() == "admin")
-                    {
-                        Console.WriteLine("Veuillez saisir le mot de passe : ");
-                        if (Console.ReadLine() == "admin")
-                        {
-                            Console.WriteLine("Vous faites parti du personnel.");
-                        }
-                    }
-                    break;
-				case 3:
-					debug();
-					break;
-                default:
-                    Console.WriteLine("Erreur 404");
-                    break;
-            }
+        //static void debug()
+        //{
+        //    Article article = new Article("Data Source=U:\\Partage\\BaseDuFastFood.sqlite;Version=3;");
+        //    // exemple pour afficher la liste d'articles
+        //    Console.WriteLine("Affichage de la liste des articles");
+        //    foreach (Article articlelist in article.GetArticles())
+        //    {
+        //        Console.WriteLine(articlelist.IDArticle + ": " + articlelist.Nom + " " + articlelist.Prix + " " + articlelist.Quantite);
+        //    }
 
-        }
-
-        static void debug()
-        {
-            Article article = new Article("Data Source=U:\\Partage\\BaseDuFastFood.sqlite;Version=3;");
-            // exemple pour afficher la liste d'articles
-            Console.WriteLine("Affichage de la liste des articles");
-            foreach (Article articlelist in article.GetArticles())
-            {
-                Console.WriteLine(articlelist.IDArticle + ": " + articlelist.Nom + " " + articlelist.Prix + " " + articlelist.Quantite);
-            }
-
-        }
+        //}
 
         static void Main(string[] args)
         {
-   //         // init database
-   //         if (!File.Exists("U:\\Partage\\BaseDuFastFood.sqlite"))
-   //         {
-   //             SQLiteConnection.CreateFile("U:\\Partage\\BaseDuFastFood.sqlite");
-   //             SQLiteConnection maConnexion = new SQLiteConnection("Data Source=BaseDuFastFood.sqlite;Version=3;");
-   //             maConnexion.Open();
-   //             string sql = "CREATE TABLE Article ('ID' INTEGER PRIMARY KEY AUTOINCREMENT, 'Nom' VARCHAR(30), 'Quantite' INT, 'Prix' DOUBLE)";
-   //             SQLiteCommand commande = new SQLiteCommand(sql, maConnexion);
-   //             commande.ExecuteNonQuery();
-   //             sql = "INSERT INTO Article ('Nom', 'Quantite', 'Prix') VALUES ('Burger', 10, 10.5)";
-   //             SQLiteCommand commande2 = new SQLiteCommand(sql, maConnexion);
-   //             commande2.ExecuteNonQuery();
-   //             maConnexion.Close();
-   //         }
+			//         // init database
+			//         if (!File.Exists("U:\\Partage\\BaseDuFastFood.sqlite"))
+			//         {
+			//             SQLiteConnection.CreateFile("U:\\Partage\\BaseDuFastFood.sqlite");
+			//             SQLiteConnection maConnexion = new SQLiteConnection("Data Source=BaseDuFastFood.sqlite;Version=3;");
+			//             maConnexion.Open();
+			//             string sql = "CREATE TABLE Article ('ID' INTEGER PRIMARY KEY AUTOINCREMENT, 'Nom' VARCHAR(30), 'Quantite' INT, 'Prix' DOUBLE)";
+			//             SQLiteCommand commande = new SQLiteCommand(sql, maConnexion);
+			//             commande.ExecuteNonQuery();
+			//             sql = "INSERT INTO Article ('Nom', 'Quantite', 'Prix') VALUES ('Burger', 10, 10.5)";
+			//             SQLiteCommand commande2 = new SQLiteCommand(sql, maConnexion);
+			//             commande2.ExecuteNonQuery();
+			//             maConnexion.Close();
+			//         }
 
 			//double ResultatMenu;
-			
-   //         Article article = new Article("Data Source=U:\\Partage\\BaseDuFastFood.sqlite;Version=3;");
-   //         //article.getArticleByID(1);
-   //         //Console.WriteLine(article.IDArticle);
-   //         //Console.WriteLine(article.Nom);
-   //         //Console.WriteLine(article.Prix);
-   //         article.GetArticles();
-			
+
+			//         Article article = new Article("Data Source=U:\\Partage\\BaseDuFastFood.sqlite;Version=3;");
+			//         //article.getArticleByID(1);
+			//         //Console.WriteLine(article.IDArticle);
+			//         //Console.WriteLine(article.Nom);
+			//         //Console.WriteLine(article.Prix);
+			//         article.GetArticles();
+
 			//foreach(Article articleList  in article.GetArticles())
 			//{
 			//	Console.WriteLine(""+articleList.IDArticle+  "     "+articleList.Nom+ "     "+articleList.Prix+ "");
 			//}
 
 			////Console.WriteLine(tab[1]);
-			affichageMenuProg();
-
 			//Menu small = new Menu();
 			//ResultatMenu = small.CalculerPrixMenu(article.GetArticles());
 			//Console.WriteLine(ResultatMenu);
-			
+
+			Fonction.affichageMenuProg();
+
 		}
     
             
