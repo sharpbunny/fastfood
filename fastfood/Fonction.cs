8e2105ab54eb4e7a9863c22098972a3b88024093
jringenbach <34011-14-08@exchange.ad.afpanet>
2017-02-02 15:03:58 +0100

    Ajout de fonction.cs

---
+++ fastfood/Fonction.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace fastfood
+{
+	/// <summary>
+	/// Classe contenant toutes les fonctions utiles au fonctionnement de VégétaFood
+	/// </summary>
+	class Fonction
+	{
+
+	}
+}
