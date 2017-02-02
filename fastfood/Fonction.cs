16a3ea82ddd259f43aa43c560239244eba3e508b
Chris.D <fusion101box@gmail.com>
2017-02-02 15:41:30 +0100

    Ajout de Création Menu

--- fastfood/Fonction.cs
+++ fastfood/Fonction.cs
@@ -11,6 +11,32 @@ namespace fastfood
 	/// </summary>
 	abstract class Fonction
 	{
+        public Menu ChoixTypeMenu()
+        {
+            Menu menu = new Menu();
+            Console.WriteLine("Quel Menu voulez vous choisir : ");
+            Console.WriteLine("1.Petit\n2.Moyen\n3.Grand");
+            typeMenu = Console.Read();
+            switch (typeMenu)
+            {
+                case 1:
+                    choixBurger();
 
-	}
+                    choixBoisson();
+                    break;
+                case 2:
+                    choixBurger();
+                    choixBoisson();
+                    choixSalade();
+                    break;
+                case 3:
+                    choixBurger();
+                    choixBoisson();
+                    choixSalade();
+                    choixGlace();
+                    break;
+            }
+            return menu;
+        }
+    }
 }
