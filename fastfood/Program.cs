9d18e96d4018e6094470ed8a679b6d5abee169dc
Chris.D <fusion101box@gmail.com>
2017-02-02 10:28:03 +0100

    Remise en ligne de l'affichage du prix

--- fastfood/Program.cs
+++ fastfood/Program.cs
@@ -53,6 +53,18 @@ namespace fastfood
 
         }
 
+        static void debug()
+        {
+            Article article = new Article("Data Source=BaseDuFastFood.sqlite;Version=3;");
+            // exemple pour afficher la liste d'articles
+            Console.WriteLine("Affichage de la liste des articles");
+            foreach (Article articlelist in article.GetArticles())
+            {
+                Console.WriteLine(articlelist.IDArticle + ": " + articlelist.Nom + " " + articlelist.Prix + " " + articlelist.Quantite);
+            }
+
+        }
+
         static void Main(string[] args)
         {
             // init database
@@ -87,29 +99,15 @@ namespace fastfood
 			//Console.WriteLine(tab[1]);
 			affichageMenuProg();
 
-
-
-			Menu small = new Menu();
+            Menu small = new Menu();
 			ResultatMenu = small.CalculerPrixMenu(article.GetArticles());
 			Console.WriteLine(ResultatMenu);
 			
 		}
-    }
+    
             
-			affichageMenuProg();
-        }
+	
 
-		static void debug()
-		{
-			Article article = new Article("Data Source=BaseDuFastFood.sqlite;Version=3;");
-			// exemple pour afficher la liste d'articles
-			Console.WriteLine("Affichage de la liste des articles");
-			foreach (Article articlelist in article.GetArticles())
-			{
-				Console.WriteLine(articlelist.IDArticle + ": " + articlelist.Nom + " " + articlelist.Prix + " " + articlelist.Quantite);
-			}
-
-		}
+		
 	}
->>>>>>> develop
 }
