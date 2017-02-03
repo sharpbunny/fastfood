f750728fa0692718d4676f5fa89945ef3bac4fab
FRANCE Frédéric <frederic.france@free.fr>
2017-02-03 08:39:35 +0100

    cleaning

--- fastfood/Program.cs
+++ fastfood/Program.cs
@@ -27,7 +27,6 @@ namespace fastfood
             Console.WriteLine("Veuillez nous indiquer si vous êtes client ou si vous faites parti du personnel :");
 			Console.WriteLine(" 1.Client");
 			Console.WriteLine(" 2.Personnel");
-			Console.WriteLine(" 3.Debug");
 
             int nombre = lireEntier();
             switch (nombre)
@@ -56,43 +55,11 @@ namespace fastfood
 
         }
 
-        static void debug()
-        {
-            Article article = new Article("Data Source=U:\\Partage\\BaseDuFastFood.sqlite;Version=3;");
-            // exemple pour afficher la liste d'articles
-            Console.WriteLine("Affichage de la liste des articles");
-            foreach (Article articlelist in article.GetArticles())
-            {
-                Console.WriteLine(articlelist.IDArticle + ": " + articlelist.Nom + " " + articlelist.Prix + " " + articlelist.Quantite);
-            }
-
-        }
 
         static void Main(string[] args)
         {
-   //         // init database
-   //         if (!File.Exists("U:\\Partage\\BaseDuFastFood.sqlite"))
-   //         {
-   //             SQLiteConnection.CreateFile("U:\\Partage\\BaseDuFastFood.sqlite");
-   //             SQLiteConnection maConnexion = new SQLiteConnection("Data Source=BaseDuFastFood.sqlite;Version=3;");
-   //             maConnexion.Open();
-   //             string sql = "CREATE TABLE Article ('ID' INTEGER PRIMARY KEY AUTOINCREMENT, 'Nom' VARCHAR(30), 'Quantite' INT, 'Prix' DOUBLE)";
-   //             SQLiteCommand commande = new SQLiteCommand(sql, maConnexion);
-   //             commande.ExecuteNonQuery();
-   //             sql = "INSERT INTO Article ('Nom', 'Quantite', 'Prix') VALUES ('Burger', 10, 10.5)";
-   //             SQLiteCommand commande2 = new SQLiteCommand(sql, maConnexion);
-   //             commande2.ExecuteNonQuery();
-   //             maConnexion.Close();
-   //         }
 
 			//double ResultatMenu;
-			
-   //         Article article = new Article("Data Source=U:\\Partage\\BaseDuFastFood.sqlite;Version=3;");
-   //         //article.getArticleByID(1);
-   //         //Console.WriteLine(article.IDArticle);
-   //         //Console.WriteLine(article.Nom);
-   //         //Console.WriteLine(article.Prix);
-   //         article.GetArticles();
 			
 
 			foreach(Article articleList  in article.GetArticles())
@@ -100,10 +67,6 @@ namespace fastfood
 				Console.WriteLine(""+articleList.IDArticle+  "     "+articleList.Nom+ "     "+articleList.Prix+ " euros");
 			}
 
-			//foreach(Article articleList  in article.GetArticles())
-			//{
-			//	Console.WriteLine(""+articleList.IDArticle+  "     "+articleList.Nom+ "     "+articleList.Prix+ "");
-			//}
 
 
 			////Console.WriteLine(tab[1]);
